b411f1606d5267d136816493feca50e84ab53924
ximanta <ximanta.sarma@gmail.com>
2019-01-14 14:57:01 +0530

    Implemented ApplicationContextAware and BeanNameAware in HelloWorld.java

--- src/main/java/com/xam/domain/HelloWorld.java
+++ src/main/java/com/xam/domain/HelloWorld.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class HelloWorld  {
+public class HelloWorld  implements ApplicationContextAware,BeanNameAware{
     private Quote quote;
+    ApplicationContext applicationContext;
 
     public HelloWorld() {
     }
@@ -19,7 +20,27 @@ public class HelloWorld  {
     public void setQuote(Quote quote) {
         this.quote = quote;
     }
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean name: "+s);
+    }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 
+     this.applicationContext=applicationContext;
+     HelloWorld helloWorld=(HelloWorld) applicationContext.getBean("helloWorld");
+     System.out.println(helloWorld);
 
+     Quote quote=(Quote) applicationContext.getBean("quote");
+     System.out.println(quote);
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorld{" +
+                "quote=" + quote +
+                ", applicationContext=" + applicationContext +
+                '}';
+    }
 }
