bdc785fb4f4fa111351214e5712f14816e88174a
ximanta <ximanta.sarma@gmail.com>
2019-01-14 11:12:34 +0530

    Updated HelloWorld to initialize with Quote

--- src/main/java/com/xam/Main.java
+++ src/main/java/com/xam/Main.java
@@ -7,24 +7,32 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class Main {
 
     public static void main(String[] args) {
-        HelloWorld obj = new HelloWorld();
-        System.out.println(obj.getMessage());
+//        HelloWorld obj = new HelloWorld();
+//        System.out.println(obj.getMessage());
 
         BeanFactory factory = new XmlBeanFactory (new ClassPathResource("beans.xml"));
         HelloWorld beanA = (HelloWorld) factory.getBean("helloWorld");
         System.out.println(beanA.getMessage());
+        System.out.println(beanA.getQuote());
 
         BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
         BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
         beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
         HelloWorld beanC=(HelloWorld) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("helloWorld");
         System.out.println(beanC.getMessage());
+        System.out.println(beanA.getQuote());
 
+        ApplicationContext applicationContext= (ApplicationContext) new ClassPathXmlApplicationContext("beans.xml");
+        HelloWorld beanD=(HelloWorld) applicationContext.getBean("helloWorld");
+        System.out.println(beanD.getMessage());
+        System.out.println(beanA.getQuote());
 
     }
 }
--- src/main/java/com/xam/domain/HelloWorld.java
+++ src/main/java/com/xam/domain/HelloWorld.java
@@ -1,9 +1,15 @@
 package com.xam.domain;
 
 public class HelloWorld {
-    public static final String HELLO_MSG = "Hello World!!!!";
-
-      public String getMessage() {
+      public static final String HELLO_MSG = "Hello World!!!!";
+      public String quote;
+    public String getQuote() {
+        return quote;
+    }
+    public HelloWorld(String quote) {
+        this.quote = quote;
+    }
+    public String getMessage() {
         return HELLO_MSG;
     }
 }
